31a0c56eb5610de3a9355a26d632db8f3108a70b
cf_jijinliang <ji_jinliang2012@163.com>
2021-03-13 16:11:55 +0800

    初始代码导入

---
+++ src/main/java/com/ky2009888/apps/MybatisPlusShowApplication.java
@@ -0,0 +1,22 @@
+package com.ky2009888.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 默认启动类
+ *
+ * @author Lenovo
+ */
+@SpringBootApplication
+public class MybatisPlusShowApplication {
+    /**
+     * 主入口运行方法
+     *
+     * @param args 命令行参数.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ky2009888/apps/MybatisPlusShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ky2009888.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
